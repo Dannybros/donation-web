34dba9a505a5032338f104b767aca6727d4c002d
Dannybros <akbaromega89@gmail.com>
2023-04-17 20:34:44 +0700

    added hero msg

--- src/components/Home/HeroSection/Hero.js
+++ src/components/Home/HeroSection/Hero.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Carousel} from 'react-bootstrap'
 import img1 from '../../../img/hero.jpg'
 import img2 from '../../../img/hero2.jpg'
+import img3 from '../../../img/hero3.jpg'
 import './Hero.scss'
 import {useNavigate} from 'react-router-dom'
 import { useTranslation } from 'react-i18next';
@@ -11,36 +12,50 @@ function HeroBoot() {
     const {t} = useTranslation();
 
     return (
+
         <Carousel>
-            {/* First Slide */}
-            <Carousel.Item 
-                interval={5000} 
-                className='Hero__container' 
-                style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${img1})`}}
-            >
-                <Carousel.Caption className="hero__msg">
-                    <h1 style={{marginBottom: "10px !important"}}>
+            <Carousel.Item>
+                <div
+                    className="hero_container"
+                    style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.4)), url(${img2})`}}
+                />
+                <Carousel.Caption className='hero_msg'>
+                    <h1 className='mb-5'>
                         {t('Home.Hero.heading1')}
                     </h1>
-                    <button onClick={()=>navigate('/project')}>
+                    <button className='hero__button my-5' onClick={()=>navigate('/project')}>
                         <span className="green"> 
                             {t('Home.Hero.button')}
                         </span>
                     </button>
                 </Carousel.Caption>
             </Carousel.Item>
-
-            {/* Second Slide */}
-            <Carousel.Item 
-                interval={5000} 
-                className='Hero__container'
-                style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.4)), url(${img2})`}}
-            >
-                <Carousel.Caption className='hero__msg'>
-                    <h1 style={{marginBottom: "10px !important"}}>
+            <Carousel.Item>
+                <div
+                    className="hero_container"
+                    style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.4)), url(${img1})`}}
+                />
+                <Carousel.Caption className='hero_msg'>
+                    <h1 className='mb-5'>
                         {t('Home.Hero.heading2')}
                     </h1>
-                    <button onClick={()=>navigate('/project')}>
+                    <button className='hero__button my-5' onClick={()=>navigate('/project')}>
+                        <span className="green"> 
+                            {t('Home.Hero.button')}
+                        </span>
+                    </button>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div
+                    className="hero_container"
+                    style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.4)), url(${img3})`}}
+                />
+                <Carousel.Caption className='hero_msg'>
+                    <h1 className='mb-5'>
+                        {t('Home.Hero.heading3')}
+                    </h1>
+                    <button className='hero__button my-5' onClick={()=>navigate('/project')}>
                         <span className="green"> 
                             {t('Home.Hero.button')}
                         </span>
